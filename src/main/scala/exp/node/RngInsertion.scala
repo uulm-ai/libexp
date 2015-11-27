579a5fdcf3ad190f23a05d4a94f6d0534132b66f
Thomas Geier <thomas.geier@uni-ulm.de>
2015-11-27 13:41:17 +0100

    properly build rng seeds

--- src/main/scala/exp/node/RngInsertion.scala
+++ src/main/scala/exp/node/RngInsertion.scala
@@ -1,22 +1,33 @@
 package exp.node
 
+import scalaz.~>
+
 /**
   * Created by thomas on 26.11.15.
   */
 case object RngInsertion extends Stage {
   override type Payload[+T] = Unit
 
-  def seed(name: String): Inject[RngInsertion.type, Long] = Inject[RngInsertion.type,Long](this,Unit, Some(name))
+  def seed(name: String): Inject[RngInsertion.type, Long] = Inject[RngInsertion.type, Long](this, Unit, Some(name))
 
   implicit def aboveBase: StageAfter[Base.type, RngInsertion.type] = StageAfter(this)
 
-  def insertSeeds[S <: Stage, T](seeds: Seq[Long], node: Node[S,T])(implicit stageCast: StageCast[S,RngInsertion.type]): Node[Base.type,T] =
+  def insertSeeds[S <: Stage, T](seeds: Seq[Long], node: Node[S, T])(implicit stageCast: StageCast[S, RngInsertion.type]): Node[Base.type, T] ={
+    val baseSeed: Node[Base.type,Long] = Base.fromSeq(seeds, "base.seed")
+    val allSeedNodes = node.allNodes.collect{
+      case i@Inject(RngInsertion, (), Some(name)) => i
+    }.toSeq.sortBy(_.name)
+
     stageCast(node) match {
-      case Inject(RngInsertion,payload,Some(name)) => Base.fromSeq(seeds,s"seed.$name").asInstanceOf[Node[Base.type,T]]
-      case Wrap(RngInsertion,n) => n.asInstanceOf[Node[Base.type,T]]
-      case App(RngInsertion,ins,f,e,n) => App(Base,ins.map(insertSeeds(seeds,_)),f,e,n)
-      case Lift(RngInsertion,p,pie,el,n) => Lift(Base,insertSeeds(seeds,p),pie,el,n)
-      case Report(RngInsertion,n,cn,f) => Report(Base,insertSeeds(seeds,n),cn,f.asInstanceOf[T => String])
+      case i@Inject(RngInsertion, (), Some(name)) =>
+        import syntax._
+        val idx = allSeedNodes.indexOf(i)
+        baseSeed.map(_ + idx, effort = Effort.none, name = s"seed.$name").asInstanceOf[BaseNode[T]]
+      case Wrap(RngInsertion, n) => n.asInstanceOf[Node[Base.type, T]]
+      case App(RngInsertion, ins, f, e, n) => App(Base, ins.map(insertSeeds(seeds, _)), f, e, n)
+      case Lift(RngInsertion, p, pie, el, n) => Lift(Base, insertSeeds(seeds, p), pie, el, n)
+      case Report(RngInsertion, n, cn, f) => Report(Base, insertSeeds(seeds, n), cn, f.asInstanceOf[T => String])
       case otherwise => sys.error(s"inexhaustive match in RngInsertion on node $otherwise")
     }
+  }
 }
